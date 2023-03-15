280ee8281d0634d860beb69cfdccd43a5d60cae4
silveiraalexandre <silveiraalexandre1993@gmail.com>
2023-03-15 20:58:39 -0300

    Inserido o banner rotativo, alterações no arquivo HTML e CSS para melhor compreensão do projeto

---
+++ js/banner-rotativo.js
@@ -0,0 +1,31 @@
+// array com as imagens do banner
+var imagensBanner = [
+    "/js/imagensBanner/img-1.jpg",
+    "/js/imagensBanner/img-2.jpg",
+    "/js/imagensBanner/img-3.jpg",
+    "/js/imagensBanner/img-4.jpg",
+    "/js/imagensBanner/img-5.jpg"
+  ];
+  
+  // intervalo de tempo para troca das imagens (em milissegundos)
+  var intervalo = 4000;
+  
+  // variável que indica o índice da imagem atual
+  var indiceAtual = 0;
+  
+  // função que exibe a próxima imagem
+  function exibirProximaImagem() {
+    // oculta a imagem atual
+    //document.getElementById("banner").style.display = "none";
+    // incrementa o índice da imagem atual
+    indiceAtual = (indiceAtual + 1) % imagensBanner.length;//
+    // exibe a próxima imagem
+    document.getElementById("imagemBanner").src = imagensBanner[indiceAtual];
+    //document.getElementById("banner").style.display = "block";
+  }
+  
+  // exibe a primeira imagem do banner
+  document.getElementById("imagemBanner").src = imagensBanner[indiceAtual];
+  
+  // define o intervalo de tempo para troca das imagens
+  setInterval(exibirProximaImagem, intervalo);
